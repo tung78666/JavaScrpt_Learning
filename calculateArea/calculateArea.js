3d823a963cc57f25a213df921f98ad1f2e5bb71f
tung78666 <ducnthe181789@fpt.edu.vn>
2024-05-19 07:55:28 -0400

    Add another method of function

--- calculateArea/calculateArea.js
+++ calculateArea/calculateArea.js
@@ -1,8 +1,14 @@
 let length;
 let width;
+//Method 1
 function calculateArea(){
     length=parseFloat(document.getElementById('length').value);
     width=parseFloat(document.getElementById('width').value);
     let area=length*width;
+
+    //Method 2
+    const cArea = () => length*width;
+    console.log(cArea());
+    
     document.getElementById('result').innerText=`The area of the rectangle is: ${area}`;
 }
